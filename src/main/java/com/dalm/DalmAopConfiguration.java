fc809f5f7672258e3434ba588236e989481f193b
Deepak Arya <deepakarya0155@gmail.com>
2023-01-24 21:16:44 +0530

    a

--- src/main/java/com/dalm/DalmAopConfiguration.java
+++ src/main/java/com/dalm/DalmAopConfiguration.java
@@ -2,6 +2,7 @@ package com.dalm;
 
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Around;
@@ -18,47 +19,62 @@ import lombok.extern.slf4j.Slf4j;
 @Aspect
 @Slf4j(topic = "AOP")
 public class DalmAopConfiguration {
-
-	@Before(value = "com.dalm.DalmAOPPointCuts.aopall()")
-	public void logBefore(JoinPoint joinPoint) {
-			String target = joinPoint.getTarget().toString();
-			String signature = joinPoint.getSignature().getName();
-
-			for (int i = 0; i < joinPoint.getArgs().length; i++) {
-				log.info("Class {}  Method {} args {} - {}", target, signature, (i + 1),
-						objectToJson(joinPoint.getArgs()[i]));
-			}
-		
-	}
-
-	@AfterReturning(value = "com.dalm.DalmAOPPointCuts.aopall()", returning = "result")
-	public void afterReturn(JoinPoint joinPoint, Object result) {
-			String target = joinPoint.getTarget().toString();
-			String signature = joinPoint.getSignature().getName();
-			log.info("Class {}  Method {} response - {}", target, signature, objectToJson(result));
-		
-	}
-
-	@AfterThrowing(value = "com.dalm.DalmAOPPointCuts.aopall()", throwing = "exception")
-	public void afterThrowing(JoinPoint joinPoint, Object exception) {
-			String target = joinPoint.getTarget().toString();
-			String signature = joinPoint.getSignature().getName();
-			log.info("Class {}  Method {} error - {}", target, signature, exception);
-		
-
-	}
+	
+//	@Before(value = "com.dalm.DalmAOPPointCuts.aopall()")
+//	public void logBefore(JoinPoint joinPoint) {
+//			String target = joinPoint.getTarget().toString();
+//			String signature = joinPoint.getSignature().getName();
+//
+//			for (int i = 0; i < joinPoint.getArgs().length; i++) {
+//				log.info("Class {}  Method {} args {} - {}", target, signature, (i + 1),
+//						objectToJson(joinPoint.getArgs()[i]));
+//			}
+//		
+//	}
+//
+//	@AfterReturning(value = "com.dalm.DalmAOPPointCuts.aopall()", returning = "result")
+//	public Object afterReturn(JoinPoint joinPoint, Object result) {
+//			String target = joinPoint.getTarget().toString();
+//			String signature = joinPoint.getSignature().getName();
+//			log.info("Class {}  Method {} response - {}", target, signature, objectToJson(result));
+//		return result;
+//	}
+//	
+//	
+//
+//	@AfterThrowing(value = "com.dalm.DalmAOPPointCuts.aopall()", throwing = "exception")
+//	public void afterThrowing(JoinPoint joinPoint, Object exception) {
+//			String target = joinPoint.getTarget().toString();
+//			String signature = joinPoint.getSignature().getName();
+//			for (int i = 0; i < joinPoint.getArgs().length; i++) {
+//				log.info("Class {}  Method {} args {} - {}", target, signature, (i + 1),
+//						objectToJson(joinPoint.getArgs()[i]));
+//			}
+//			log.info("Class {}  Method {} error - {}", target, signature, exception);
+//		
+//
+//	}
 
 	@Around(value = "com.dalm.DalmAOPPointCuts.aopall()")
-	public void around(ProceedingJoinPoint joinPoint) throws Throwable {
-		
+	public Object around(ProceedingJoinPoint joinPoint) throws Throwable{
 			String target = joinPoint.getTarget().toString();
 			String signature = joinPoint.getSignature().getName();
 			Long startTime = System.currentTimeMillis();
-			joinPoint.proceed();
-			Long endTime = System.currentTimeMillis();
-			log.info("Class {}  Method {} time - {}", target, signature, (endTime - startTime));
-		
-
+			
+			
+			try {
+				for (int i = 0; i < joinPoint.getArgs().length; i++) {
+					log.info("Class {}  Method {} args {} - {}", target, signature, (i + 1),
+							objectToJson(joinPoint.getArgs()[i]));
+				}
+				return joinPoint.proceed();
+			} catch (Throwable e) {
+				throw e;
+			}finally {
+				Long endTime = System.currentTimeMillis();
+				log.info("Class {}  Method {} time - {}", target, signature, (endTime - startTime));
+			}
+			
 	}
 
 	public String objectToJson(Object obj) {
@@ -67,9 +83,13 @@ public class DalmAopConfiguration {
 			return mapper.writer().writeValueAsString(obj);
 		} catch (JsonProcessingException e) {
 			// TODO Auto-generated catch block
-			e.printStackTrace();
+			try {
+				return obj.toString();
+			}catch(Exception e1) {
+				return e1.getMessage();
+			}
 		}
-		return null;
+		
 	}
 
 }
